28d3e95a139e521206b5abb2cd28a615264e182b
StarAppeal <silkfan11@gmail.com>
2024-07-23 13:20:44 +0200

    add and fix tests

--- src/test/java/de/starappeal/versicherung/utils/CsvServiceTest.java
+++ src/test/java/de/starappeal/versicherung/utils/CsvServiceTest.java
@@ -2,7 +2,9 @@ package de.starappeal.versicherung.utils;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import de.starappeal.versicherung.entities.VehicleType;
 import de.starappeal.versicherung.utils.CsvService.RegionRow;
+import de.starappeal.versicherung.utils.CsvService.VehicleTypeRow;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -12,22 +14,34 @@ class CsvServiceTest {
 
   private CsvService csvService;
 
-  @BeforeEach
-  void setup() {
-    this.csvService = new CsvService("src/test/resources/test.csv");
-  }
-
   @Test
-  void readRows() {
+  void readRows_Region() {
+    this.csvService = new CsvService("src/test/resources/regions.csv");
     List<RegionRow> regions = csvService.readRows(CsvService.regionParser);
+    assertEquals(7, regions.size());
+
     RegionRow firstRegion = regions.get(0);
 
     assertEquals("Baden-Württemberg", firstRegion.state());
-    assertEquals("Breisgau-Hochschwarzwald", firstRegion.country());
+    assertEquals("DE", firstRegion.country());
     assertEquals("Bad Krozingen", firstRegion.city());
     assertEquals("79189", firstRegion.zipCode());
-    assertEquals("Bad Krozingen", firstRegion.location());
+    assertEquals("Breisgau-Hochschwarzwald", firstRegion.location());
 
     System.out.println(regions);
+  }
+
+  @Test
+  void readRows_VehicleType() {
+    this.csvService = new CsvService("src/test/resources/vehicles.csv");
+
+    List<VehicleTypeRow> vehicleTypes = csvService.readRows(CsvService.vehicleTypeParser);
+
+    assertEquals(2, vehicleTypes.size());
+
+    VehicleTypeRow firstVehicleType = vehicleTypes.get(0);
+
+    assertEquals("Test", firstVehicleType.vehicleName());
+
   }
 }
